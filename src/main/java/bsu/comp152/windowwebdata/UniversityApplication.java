7b637b8174da2e62723d13c5fac60ff65031737c
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-11-09 10:33:56 -0500

    Loading university information menu

--- src/main/java/bsu/comp152/windowwebdata/UniversityApplication.java
+++ src/main/java/bsu/comp152/windowwebdata/UniversityApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class UniversityApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(UniversityApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(UniversityApplication.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("University Information");
         stage.setScene(scene);
         stage.show();
     }
