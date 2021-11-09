b5d74eb093384e8c374bd8a88c2e42eb3e3ca751
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-11-09 10:40:38 -0500

    Adding exceptions

--- src/main/java/bsu/comp152/windowwebdata/UniversityApplication.java
+++ src/main/java/bsu/comp152/windowwebdata/UniversityApplication.java
@@ -11,7 +11,13 @@ public class UniversityApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UniversityApplication.class.getResource("Main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = null;
+        try {
+            scene = new Scene(fxmlLoader.load(), 600, 400);
+        }
+        catch (IOException e){
+            System.out.println("Error loading FXML or exception in the controller class.");
+        }
         stage.setTitle("University Information");
         stage.setScene(scene);
         stage.show();
